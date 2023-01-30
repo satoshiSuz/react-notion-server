0ffd701b8acca74e5d30341c9b4e5bbe25f42981
satoshi.suzuki <arusta02@gmail.com>
2023-01-31 00:55:23 +0900

    21 webAPItest

---
+++ src/v1/models/user.ts
@@ -0,0 +1,21 @@
+import { Schema, model, Document } from 'mongoose';
+
+export interface IUser extends Document {
+  username: string;
+  password: string;
+}
+
+export const userSchema = new Schema<IUser>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = model<IUser>('User', userSchema);
+export default User;
